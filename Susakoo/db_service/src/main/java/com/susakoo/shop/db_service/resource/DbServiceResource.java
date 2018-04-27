0cd4e8b82e2042a8c9e8dacac892381b51b8d92c
Lencho, Segni <segni.lencho@intelligrated.com>
2018-04-27 14:28:28 -0400

    Swagger_Page implemented and integrated to both db_service and order_service

--- Susakoo/db_service/src/main/java/com/susakoo/shop/db_service/resource/DbServiceResource.java
+++ Susakoo/db_service/src/main/java/com/susakoo/shop/db_service/resource/DbServiceResource.java
@@ -1,14 +1,14 @@
 package com.susakoo.shop.db_service.resource;
 
-import com.susakoo.shop.db_service.domain.Book;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.susakoo.shop.db_service.repository.StockRepository;
+        import com.susakoo.shop.db_service.domain.Book;
+        import java.util.List;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestBody;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import com.susakoo.shop.db_service.repository.StockRepository;
 
 @RestController
 @RequestMapping("rest/db")
